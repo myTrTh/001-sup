1bfa7b60011d02bd332c542647acbb64a02354a0
ssVirth <ssvirth@mail.ru>
2018-05-31 14:00:45 +0300

    create converter from ten to two and eight and conversely

--- tentotwo.php
+++ tentotwo.php
@@ -4,7 +4,7 @@ echo 'Системы счисления<br>';
 
 class Convert
 {
-	public function converter(int $item, int $system)
+	public function converterFromTen(int $item, int $system)
 	{
 		/*
 		Получаем остаток от деления на два 1 или 0, записываем
@@ -22,11 +22,31 @@ class Convert
 			$toStringStack .= implode($stack);
 			echo $toStringStack;
 		} else {
-			echo 'Основание может быть 2 или 8, вы указали основание: '.$system;
+			echo '<br>Основание может быть 2 или 8, вы указали основание: '.$system;
+		}
+	}
+
+	public function converterToTen(int $item, int $system)
+	{
+		if ($system == 2 || $system == 8) {		
+			$arr = str_split($item);
+			$arr = array_reverse($arr);
+			$sum = 0;
+			for ($i=0; $i < count($arr) ; $i++) {
+				$rem = $arr[$i] * pow($system, $i);
+				$sum += $rem;				
+			}
+			$toStringStack = '<br><br>'.$item.' в системе счисления '.$system.' по основанию 10 будет '.$sum;
+			echo $toStringStack;
+		} else {
+			echo '<br>Основание может быть 2 или 8, вы указали основание: '.$system;
 		}
 	}
 }
 
 $translator = new Convert();
-$translator->converter(233, 8);
-$translator->converter(233, 2);
+$translator->converterFromTen(233, 8);
+$translator->converterFromTen(233, 2);
+
+$translator->converterToTen(11101001, 2);
+$translator->converterToTen(351, 8);
